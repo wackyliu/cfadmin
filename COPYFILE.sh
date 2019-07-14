5d410e482339de7a6b14e2f89ca2b821b94fe45f
CandyMi <869646063@qq.com>
2019-07-14 14:11:17 +0800

    增加一键copy文件到build的执行文件

--- COPYFILE.sh
+++ COPYFILE.sh
@@ -1,3 +1,12 @@
+# copy需要打包的文件
+cp ../3rd ./
+cp ../logs ./
+cp ../lualib ./
+cp ../script ./
+
+mkdir luaclib && mv *.dll luaclib/ && mv luaclib/*core.dll ./
+
+# copy系统依赖文件
 cp /usr/bin/msys-2.0.dll ./
 cp /usr/bin/msys-z.dll ./
 cp /usr/bin/msys-ssl-*.dll ./
