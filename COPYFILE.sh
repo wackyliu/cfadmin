22567346c233a6fb01cac3a6f2d675e31de69cb5
CandyMi <869646063@qq.com>
2019-08-21 05:05:17 +0800

    精简复制文件脚本

--- COPYFILE.sh
+++ COPYFILE.sh
@@ -2,16 +2,9 @@
 rm -rf *.a *make* *Make*
 
 # copy需要打包的文件
-cp -R ../3rd ./
-cp -R ../logs ./
-cp -R ../lualib ./
-cp -R ../script ./
-cp -R ../static ./
+cp -R ../3rd ../logs ../lualib ../script ../static ../docker ./
 
 mkdir luaclib && mv *.dll luaclib/ && mv luaclib/*core.dll ./
 
 # copy系统依赖文件
-cp /usr/bin/msys-2.0.dll ./
-cp /usr/bin/msys-z.dll ./
-cp /usr/bin/msys-ssl-*.dll ./
-cp /usr/bin/msys-crypto-*.dll ./
+cp /usr/bin/msys-2.0.dll /usr/bin/msys-z.dll /usr/bin/msys-ssl-*.dll /usr/bin/msys-crypto-*.dll ./
