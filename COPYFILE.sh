e00ee76c27e8260aae58db09173c72780cbc00b7
869646063@qq.com <869646063@qq.com>
2020-02-29 01:02:00 +0800

    更新代码

--- COPYFILE.sh
+++ COPYFILE.sh
@@ -4,7 +4,7 @@ rm -rf *.a *make* *Make*
 # copy需要打包的文件
 cp -R ../3rd ../logs ../lualib ../script ../static ../docker ./
 
-mkdir luaclib && mv *.so luaclib && mv luaclib/*core* .
+mkdir luaclib && mv *.dll luaclib && mv luaclib/*core* .
 
 # copy系统依赖文件
 cp /usr/bin/msys-2.0.dll /usr/bin/msys-z.dll /usr/bin/msys-ssl-*.dll /usr/bin/msys-crypto-*.dll ./
