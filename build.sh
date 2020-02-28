d702eb65a0804582a7490ee0fe56f0b499505dc2
869646063@qq.com <869646063@qq.com>
2020-02-29 01:40:41 +0800

    更新编译构建脚本

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ cp -R ../3rd ../logs ../lualib ../script ../static ../docker ./
 mkdir luaclib && mv *.dll luaclib && mv luaclib/*core* .
 
 # copy测试代码依赖的文件
-cp ../*.pem ../*.csv ../*.pb .
+cp ../*.pem ../*.csv ../Person* .
 
 # copy系统依赖文件
 cp /usr/bin/msys-2.0.dll /usr/bin/msys-z.dll /usr/bin/msys-ssl-*.dll /usr/bin/msys-crypto-*.dll ./
